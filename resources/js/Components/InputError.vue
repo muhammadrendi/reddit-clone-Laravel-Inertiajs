021f9d7d4ddfd76c2173880ea92993400d11bb67
muhammadrendi <muhammadrendi360@gmail.com>
2022-08-12 14:22:08 +0700

    Prepare Project

--- resources/js/Components/InputError.vue
+++ resources/js/Components/InputError.vue
@@ -3,9 +3,27 @@ defineProps(['message']);
 </script>
 
 <template>
-    <div v-show="message">
-        <p class="text-sm text-red-600">
-            {{ message }}
-        </p>
-    </div>
+    <Transition name="slide-fade">
+        <div v-show="message">
+            <p class="inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-red-100 bg-red-600 rounded-full">
+                {{ message }}
+            </p>
+        </div>
+    </Transition>
 </template>
+
+<style scoped>
+    .slide-fade-enter-active {
+        transition: all 0.3s ease-out;
+    }
+
+    .slide-fade-leave-active {
+    transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
+    }
+
+    .slide-fade-enter-from,
+    .slide-fade-leave-to {
+    transform: translateY(10px);
+    opacity: 0;
+    }
+</style>
